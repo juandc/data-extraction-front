5f0e7a1a8ef671cd703422723ec8d71a1ea54077
Juan David Castro Gallego <fjuandc@gmail.com>
2025-01-25 17:33:17 -0500

    fix: drop files o take webcam photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,84 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import Image from "next/image";
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { WebcamCapture } from "@/components/WebcamCapture";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div>
-          Arrastra tu archivo o toma una foto
-        </div>
+const fileTypes = ["JPEG", "PNG", "GIF", "PDF", "DOC", "DOCX"];
 
-        <div className={styles.ctas}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.primary}
-          >
-            Enviar
-          </a>
+export default function Home() {
+  const [files, setFiles] = useState<File[]>([]);
+  const [takingPhoto, setTakingPhoto] = useState<boolean>(false);
+
+  const handleFile = (fileList: FileList) => {
+    const file = Array.from(fileList);
+    setFiles(prev => [...prev, ...file]);
+  };
+
+  const addSs = (file: Base64URLString) => {
+    const newFile = new File([file], `webcam-${Date.now()}.jpg`, { type: "image/jpeg" });
+    setFiles(prev => [...prev, newFile]);
+    setTakingPhoto(false);
+  };
+
+  let label: string | undefined;
+  if (!files?.length) label = "Arrastra o selecciona tu archivo";
+  else label = "Arrastrar o seleccionar más archivos";
+
+  return (
+    <div>
+      {takingPhoto && (
+        <WebcamCapture addSs={addSs} />
+      )}
+
+      {!takingPhoto && (
+        <div className={styles.page}>
+          <main className={styles.main}>
+            <div className={styles.dropzone}>
+              <FileUploader
+                multiple={true}
+                name="file"
+                handleChange={handleFile}
+                fileTypes={fileTypes}
+                classes={styles.dropzone_drag}
+              >
+                <p>{label}</p>
+              </FileUploader>
+
+              <button
+                type="button"
+                className={styles.dropzone_photo}
+                onClick={() => setTakingPhoto(true)}
+              >
+                Toma una foto
+              </button>
+            </div>
+
+            {files && (
+              <div>
+                {[...files].flatMap((file, index) => (
+                  <p key={`${index}`}>{file.name}</p>
+                ))}
+              </div>
+            )}
+
+            <div className={styles.ctas}>
+              <a
+                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.primary}
+              >
+                Enviar
+              </a>
+            </div>
+          </main>
         </div>
-      </main>
-      <footer className={styles.footer}>
+      )}
+
+      {/* <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
@@ -65,7 +121,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
---
+++ src/components/WebcamCapture.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useRef } from "react";
+import Webcam from "react-webcam";
+import styles from "../app/page.module.css";
+
+type Props = {
+  addSs: (file: Base64URLString) => void;
+};
+
+export const WebcamCapture = ({ addSs }: Props) => {
+  const webcamRef = useRef<any>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    console.log(webcamRef, imageSrc);
+    addSs(imageSrc);
+  }, [webcamRef, addSs]);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100vw" }}>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 32,
+        position: "relative",
+        height: "100%",
+        width: "100%",
+      }}>
+        <Webcam
+          audio={false}
+          height={720 / 4}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={1280 / 4}
+          videoConstraints={{
+            aspectRatio: 0.5625,
+            height: 1280 / 4,
+            width: 720 / 4,
+          }}
+          style={{
+            borderRadius: 32,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1,
+            height: 1280 / 2,
+            width: 720 / 2,
+          }}
+        />
+        <button
+          className={styles.primary}
+          onClick={capture}
+        >Capture photo</button>
+      </div>
+    </div>
+  );
+};
